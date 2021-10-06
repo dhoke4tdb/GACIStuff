e6c7f98621b581c1cdcf5e2c36b13d9ca28d88f9
dhoke4tdb <david.hoke@tiledb.com>
2021-10-06 04:56:23 -0400

    attempt set-output from program writing to stdout

---
+++ src/setoutput/setoutput.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("::set-output name=TILEDB_CI_W2016_SUCCESS::1\n");
+  printf("::set-output name=TILEDB_CI_W2019_SUCCESS::1\n");
+  printf("::set-output name=TILEDB_CI_U2004_SUCCESS::1\n");
+
+  return 0 ;
+}
